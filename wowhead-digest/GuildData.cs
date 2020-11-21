25dce3d15f372093eb2c4e5ac8f9bc6a9f4a9595
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-11-21 01:10:08 -0800

    Removed extraneous GuildData usings

--- wowhead-digest/GuildData.cs
+++ wowhead-digest/GuildData.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
 
 using DSharpPlus;
-using DSharpPlus.Entities;
 
 namespace wowhead_digest {
 	class GuildData {
