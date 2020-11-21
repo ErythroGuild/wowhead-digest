ca75aadc96dd151b6299b16c849f4ad51d636885
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-11-21 06:00:05 -0800

    Implemented basic command flow
    + some easy to implement commands

--- wowhead-digest/Program.cs
+++ wowhead-digest/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -11,13 +11,16 @@ namespace wowhead_digest {
 	class Program {
 		static readonly Logger log = new Logger();
 		static DiscordClient discord = null;
+		static string str_mention = null;
 
-		static Dictionary<DiscordGuild, GuildData> guildData;
+		static Dictionary<DiscordGuild, GuildData> guildData =
+			new Dictionary<DiscordGuild, GuildData>();
 
 		const string path_token    = @"token.txt";
 		const string path_settings = @"settings.txt";
 		const string path_digests  = @"digests.txt";
 
+		const ulong id_u_self = 779053723489140759;
 		const ulong id_wh_wowhead = 779070312561770528;
 		const ulong id_ch_ingest = 777935219193020426;	// <Erythro> - #ingest
 		const ulong id_ch_debug = 489274692255875091;	// <Erythro> - #test
@@ -42,9 +45,11 @@ namespace wowhead_digest {
 				return;
 			}
 
-			discord.Ready += (s, e) => {
+			discord.Ready += async (s, e) => {
 				log.Info("Connected to discord.");
-				return Task.CompletedTask;
+
+				str_mention = (await discord.GetUserAsync(id_u_self)).Mention;
+				log.Debug("  Self mention string: " + str_mention);
 			};
 
 			discord.GuildDownloadCompleted += async (s, e) => {
@@ -131,24 +136,34 @@ namespace wowhead_digest {
 				}
 				log.Info("Guild settings parsed.");
 
-				log.Info("Writing back loaded data...");
-				StreamWriter writer_settings = new StreamWriter(path_settings);
-				StreamWriter writer_digests = new StreamWriter(path_digests);
-				foreach (DiscordGuild guild in guildData.Keys) {
-					string guild_str = "GUILD - " + guild.Id.ToString();
-					writer_settings.WriteLine(guild_str);
-					writer_settings.Write(guildData[guild].settings.Save());
-					writer_settings.WriteLine();
-					writer_digests.WriteLine(guild_str);
-					writer_digests.Write(guildData[guild].digests.ToString());
-					writer_digests.WriteLine();
-				}
-				writer_settings.Close();
-				writer_digests.Close();
-				log.Info("Data written.");
+				log.Info("Writing back loaded data:");
+				Save();
 			};
 
 			discord.MessageCreated += async (s, e) => {
+				if (e.Message.Content.StartsWith(str_mention)) {
+					log.Info("Command detected.");
+					log.Debug(e.Message.Content);
+					string message = e.Message.Content.Substring(str_mention.Length + 1);
+					string[] split = message.Split(' ', 2);
+					string cmd, arg;
+					if (message.Contains(' ')) {
+						cmd = split[0];
+						arg = split[1];
+					} else {
+						cmd = message;
+						arg = "";
+					}
+					log.Debug("  cmd: " + cmd);
+					log.Debug("  arg: " + arg);
+					string reply = ParseCommand(cmd, arg, e.Guild);
+					DiscordChannel ch_reply = e.Channel;
+#if DEBUG
+					ch_reply = await discord.GetChannelAsync(id_ch_debug);
+#endif
+					_ = discord.SendMessageAsync(ch_reply, reply);
+				}
+
 				// Must be webhook message & author must be Wowhead webhook.
 				if (!e.Message.WebhookMessage || e.Message.WebhookId != id_wh_wowhead)
 					return;
@@ -161,6 +176,8 @@ namespace wowhead_digest {
 					string url = embed.Url.AbsoluteUri;
 					string id = Article.UrlToId(url);
 					Article article = new Article(id, DateTime.Now);
+
+					log.Info("New article posted!");
 
 					//foreach (DiscordGuild guild in discord.Guilds.Values) {
 					//	guildData[guild].Push(article);
@@ -210,6 +227,169 @@ namespace wowhead_digest {
 				TokenType = TokenType.Bot
 			});
 			log.Info("Connecting to discord...");
+		}
+
+		static void Save() {
+			log.Info("Saving data...");
+			StreamWriter writer_settings = new StreamWriter(path_settings);
+			StreamWriter writer_digests = new StreamWriter(path_digests);
+			foreach (DiscordGuild guild in guildData.Keys) {
+				string guild_str = "GUILD - " + guild.Id.ToString();
+				writer_settings.WriteLine(guild_str);
+				writer_settings.Write(guildData[guild].settings.Save());
+				writer_settings.WriteLine();
+				writer_digests.WriteLine(guild_str);
+				writer_digests.Write(guildData[guild].ExportDigests());
+				writer_digests.WriteLine();
+			}
+			writer_settings.Close();
+			writer_digests.Close();
+			log.Info("Data saved.");
+		}
+
+		static string ParseCommand(string cmd, string arg, DiscordGuild guild) {
+			const string str_cmd_listSettings		= "list-settings";
+			const string str_cmd_listCategories		= "list-categories";
+			const string str_cmd_listSeries			= "list-series";
+			const string str_cmd_listOverrides		= "list-overrides";
+			const string str_cmd_setNews			= "set-news";
+			const string str_cmd_setLogs			= "set-logs";
+			const string str_cmd_setFreq			= "set-freq";
+			const string str_cmd_setDetectSpoilers	= "set-auto-spoilers";
+			const string str_cmd_setCensorSpoilers	= "set-censor-spoilers";
+
+			string output = "";
+			Settings settings = guildData[guild].settings;
+			switch (cmd) {
+			case str_cmd_listSettings:
+				output = "**<" + guild.Name + "> Settings**" + "\n";
+				output += "\u2003" + "News Channel: " +
+					settings.ch_news.Mention + "\n";
+				output += "\u2003" + "Logs Channel: " +
+					settings.ch_logs.Mention + "\n";
+				output += "\u2003" + "Posting digests ";
+				switch (settings.postFrequency) {
+				case Settings.PostFrequency.Daily:
+					output += "*daily*." + "\n";
+					break;
+				case Settings.PostFrequency.Weekly:
+					output += "*weekly*." + "\n";
+					break;
+				}
+				output += "\u2003" + "Spoiler post titles are ";
+				switch (settings.doCensorSpoilers) {
+				case true:
+					output += "*hidden*." + "\n";
+					break;
+				case false:
+					output += "*shown*." + "\n";
+					break;
+				}
+				output += "\u2003";
+				switch (settings.doDetectSpoilers) {
+				case true:
+					output += "Will try to automatically detect spoilers." + "\n";
+					break;
+				case false:
+					output += "Spoilers must be marked manually." + "\n";
+					break;
+				}
+				break;
+			case str_cmd_listCategories:
+				Dictionary<Article.Category, bool> doShowCategory = settings.doShowCategory;
+				output = "**<" + guild.Name + "> Category Filters**" + "\n";
+				foreach(Article.Category category in doShowCategory.Keys) {
+					output += "\u2003";
+					switch (doShowCategory[category]) {
+					case true:
+						output += ":white_check_mark: " +
+							category.ToString() + "\n";
+						break;
+					case false:
+						output += ":no_entry_sign: ~~" +
+							category.ToString() + "~~" + "\n";
+						break;
+					}
+				}
+				break;
+			case str_cmd_listSeries:
+				Dictionary<Article.Series, bool> doShowSeries = settings.doShowSeries;
+				output = "**<" + guild.Name + "> Series Filters**" + "\n";
+				foreach (Article.Series series in doShowSeries.Keys) {
+					output += "\u2003";
+					switch (doShowSeries[series]) {
+					case true:
+						output += ":white_check_mark: " +
+							series.ToString() + "\n";
+						break;
+					case false:
+						output += ":no_entry_sign: ~~" +
+							series.ToString() + "~~" + "\n";
+						break;
+					}
+				}
+				break;
+			case str_cmd_listOverrides:
+				output = "**<" + guild.Name + "> Filter Overrides**" + "\n";
+				output += "Hide Articles:" + "\n";
+				if (settings.articles_hidden.Count == 0) {
+					output += "\u2003" + "*none*" + "\n";
+				} else {
+					foreach (Article article in settings.articles_hidden) {
+						output += "\u2003" + "<" + article.url + ">" + "\n";
+					}
+				}
+				output += "Show Articles:" + "\n";
+				if (settings.articles_shown.Count == 0) {
+					output += "\u2003" + "*none*" + "\n";
+				} else {
+					foreach (Article article in settings.articles_shown) {
+						output += "\u2003" + "<" + article.url + ">" + "\n";
+					}
+				}
+				output += "Spoiler Articles:" + "\n";
+				if (settings.articles_spoilered.Count == 0) {
+					output += "\u2003" + "*none*" + "\n";
+				} else {
+					foreach (Article article in settings.articles_spoilered) {
+						output += "\u2003" + "<" + article.url + ">" + "\n";
+					}
+				}
+				output += "Un-spoiler Articles:" + "\n";
+				if (settings.articles_unspoilered.Count == 0) {
+					output += "\u2003" + "*none*" + "\n";
+				} else {
+					foreach (Article article in settings.articles_unspoilered) {
+						output += "\u2003" + "<" + article.url + ">" + "\n";
+					}
+				}
+				break;
+			case str_cmd_setNews:
+				break;
+			case str_cmd_setLogs:
+				break;
+			case str_cmd_setFreq:
+				Dictionary<string, Settings.PostFrequency> strToPostFreq =
+					new Dictionary<string, Settings.PostFrequency> {
+						{ "daily", Settings.PostFrequency.Daily },
+						{ "day"  , Settings.PostFrequency.Daily },
+						{ "d"    , Settings.PostFrequency.Daily },
+						{ "1"    , Settings.PostFrequency.Daily },
+						{ "weekly", Settings.PostFrequency.Weekly },
+						{ "week"  , Settings.PostFrequency.Weekly },
+						{ "w"     , Settings.PostFrequency.Weekly },
+						{ "7"     , Settings.PostFrequency.Weekly },
+					};
+				Settings.PostFrequency postFreq = strToPostFreq[arg.ToLower()];
+				guildData[guild].settings.postFrequency = postFreq;
+				Save();
+				output += "Post frequency set to *" +
+					postFreq.ToString().ToLower() +
+					"*." + "\n";
+				break;
+			}
+
+			return output;
 		}
 	}
 }
